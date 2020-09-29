430d9d536cd14e969679eac0fc1fe8d9fb7ae6c8
camilomontoyau <camilomontoya2@gmail.com>
2020-09-29 14:29:25 -0500

    testFechaEsString

--- tests/FechaTest.php
+++ tests/FechaTest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 final class FechaTest extends TestCase
 {
-    public function testIsString() {
+    public function testFechaEsString() {
         $mifecha = new Fecha('20/09/2020');
         $this->assertTrue(is_string($mifecha->fecha));
     }
