4fdda4b97bef5f5d09112b429e8fe30fb05718ef
camilomontoyau <camilomontoya2@gmail.com>
2020-10-29 13:17:33 -0500

    consulta

--- lib/src/Consulta.php
+++ lib/src/Consulta.php
@@ -1,13 +1,33 @@
 <?php
-class Consulta
-{
-    public $fecha;
-    public $paciente;
+require "Fecha.php";
+require "Paciente.php";
 
+class Consulta{
+	private $objPaciente;
+	private $objFecha;	
 
-    public function __construct($paciente) {
-        $nuevaFecha = date('dd/mm/yyyy');
-        $this->fecha = new Fecha($nuevaFecha);
-        $this->paciente = new Paciente($paciente);
+	public function __construct($id, $nombre, $apellido, $fecha)
+    {           
+        $objFecha = new Fecha($fecha);        
+       	$objPaciente = new Paciente($id, $nombre, $apellido);
+    }
+
+    public function getPaciente(){
+    	return $this->objPaciente;
+    }
+
+    public function getFecha(){
+    	return $this->objFecha;
+    }
+
+    public function consultaEsValida(){
+        $id = "1";
+        $nombre = "Juan";
+        $apellido = "Perez";
+        $fecha = "01/02/2020";
+        $objFecha = new Fecha($fecha);        
+        $objPaciente = new Paciente($id, $nombre, $apellido);
+        return $objPaciente->pacienteEsValido($id, $nombre, $apellido) && $objFecha->fechaEsValida($fecha);
     }
 }
+?>
