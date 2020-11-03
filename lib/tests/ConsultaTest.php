081562dca02b240b0a0cc4ea4badcb39b46329dc
camilomontoyau <camilomontoya2@gmail.com>
2020-11-03 13:39:28 -0500

    consultaTest

--- lib/tests/ConsultaTest.php
+++ lib/tests/ConsultaTest.php
@@ -1,6 +1,10 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-final class ConsultaTest extends TestCase {
-    
+final class ConsultaTest extends TestCase
+{
+    public function testPuedeCrearConsulta() {
+      $consulta = new Consulta('id', 'nombre', 'apellido', '20/02/2020');
+      $this->assertInstanceOf(Consulta::class, $consulta);
+    }    
 }
