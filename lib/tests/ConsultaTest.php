ef33dfbe5bca7d36c22007abc8d0344efae1f5ba
camilomontoyau <camilomontoya2@gmail.com>
2020-11-05 15:30:49 -0500

    testPostCrearConsultaMuestraErrorSinId terminado

--- lib/tests/ConsultaTest.php
+++ lib/tests/ConsultaTest.php
@@ -28,19 +28,18 @@ final class ConsultaTest extends TestCase
     
     public function testPostCrearConsultaMuestraErrorSinId() {
       $client = new Client();
-      $response = $client->post('http://localhost', [
-        'json' => [ 
-          'id'=> '',
-          'nombre'=> 'nombre',
-          'apellido'=> 'apellido'
-        ]
-      ]);
-      var_dump($response->getStatusCode());
-      $this->assertEquals($response->getStatusCode(), 400);
-      //$json_consulta = json_decode($response->getBody());
-      /* $this->assertEquals($json_consulta->paciente->nombre, 'nombre');
-      $this->assertEquals($json_consulta->paciente->apellido, 'apellido');
-      $this->assertEquals($json_consulta->paciente->id, 'id');
-      $this->assertEquals($json_consulta->fecha->fecha, date("d/m/Y")); */
+      try {
+        $response = $client->post('http://localhost', [
+          'json' => [ 
+            'id'=> '',
+            'nombre'=> 'nombre',
+            'apellido'=> 'apellido'
+          ]
+        ]);
+      } catch (Exception $error) {
+        $mensaje = $error->getMessage();
+        $this->assertTrue(strlen(strstr($mensaje, 'Falta id')) > 0);
+        $this->assertTrue(strlen(strstr($mensaje, '400')) > 0);
+      }
     }    
 }
